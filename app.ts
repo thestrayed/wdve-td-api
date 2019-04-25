1d14e4163e29d8797b46c77b9f41fb0ffcec2695
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-04-25 11:08:56 +0700

    updated files

--- app.ts
+++
@@ -1,8 +0,0 @@
-import config from 'config';
-import express from 'express';
-
-const app = express();
-
-app.set('port', config.get('wdve-td.port'));
-
-export default app;
--- server.ts
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-app.listen(app.get('port'), () => {
-    console.log(`Server is listening on port: ${app.get('port')}`);
-});
