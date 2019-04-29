7d2925499df600b86da0b2c80afdc8fd55282e4d
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-04-29 18:09:25 +0700

    add simple controller

--- server/controllers/place/all.ts
+++ server/controllers/place/all.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import HttpStatus from 'http-status';
 
-import db from '@models';
-import { PlaceModelStatic } from '@typings';
+import { PlaceService } from '@services';
+
+const placeService = new PlaceService();
 
 export default async (_req: Request, res: Response) => {
     try {
-        const places = await (db.Place as PlaceModelStatic).findAll();
+        const places = await placeService.all();
         res.status(HttpStatus.OK).json(places);
     } catch (err) {
         throw err;
     }
-}
+};
