d697be533861f4985969a16d88a69b0e937c7fe4
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-04-30 12:31:07 +0700

    remove test endpoint

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,8 @@
 import config from 'config';
 import express, { Request, Response } from 'express';
 
-import db from './models';
-import { PlaceModelStatic } from './typings';
-
 const app = express();
 
 app.set('port', config.get('wdve-td.port'));
-
-app.get('/', async (_req: Request, res: Response) => {
-    try {
-        const places = await (db.Place as PlaceModelStatic).findAll();
-        res.status(200).json(places);
-    } catch (err) {
-        console.error(err);
-    }
-});
 
 export default app;
