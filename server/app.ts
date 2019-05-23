19967090129676e9559b1b296cb7a1cd99b0289b
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-23 15:19:51 +0700

    add error handler

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,12 @@
 import config from 'config';
-import express, { Request, Response } from 'express';
+import express from 'express';
+
+import { errorHandler } from '@middlewares';
 
 const app = express();
 
 app.set('port', config.get('wdve-td.port'));
+
+app.use(errorHandler);
 
 export default app;
