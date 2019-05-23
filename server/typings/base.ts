ba731d73f4c8c67c5965ad4c44e419e7ec291ba8
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-23 15:27:07 +0700

    add optionalizer and IBaseService in base

--- server/typings/base.ts
+++ server/typings/base.ts
@@ -1,13 +1,34 @@
 import { BuildOptions, Model } from 'sequelize';
 
+import { ModelCreateOptions, ModelUpdateOptions } from './db';
+
 export interface BaseModel<T> extends Model<T>  {
-    id: string;
     createdAt: string;
     updatedAt: string;
 }
 
+export interface BaseModelAttributes {
+    id: number;
+}
+
 export type BaseModelStatic<T> = typeof Model & {
     new(values?: object, options?: BuildOptions): T;
+};
+
+/**
+ * @template BaseModel
+ * @template Optionalize
+ */
+export interface IBaseService<BaseModel, Optionalize> {
+    create(createObj: Optionalize, options: ModelCreateOptions): Promise<BaseModel>;
+    get(): Promise<BaseModel[]>;
+    limit(number: number): this;
+    update(updateObj: Optionalize, options: ModelUpdateOptions): Promise<BaseModel>;
+    where(whereObj: Optionalize): this;
+}
+
+export type Optionalize<T> = {
+    [K in keyof T]?: T[K];
 };
 
 export interface SoftDeleteBaseModel {
