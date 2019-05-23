8d9faeb287ebf330991c948da0ff94c25a421729
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-23 15:32:18 +0700

    update mock to return jest.fn to further stub the request

--- server/__mocks__/@models/index.ts
+++ server/__mocks__/@models/index.ts
@@ -1,11 +1,12 @@
+import faker from 'faker';
+import _ from 'lodash';
+
+import { PartialPlace } from '@typings/models/place';
+
 export default {
     Place: {
-        findByPk: (id: number) => Promise.resolve({
-            id: id,
-            name: 'place',
-            latitude: '100',
-            longtitude: '100',
-        }),
-        findAll: () => Promise.resolve([]),
-    }
+        create: (createObj: PartialPlace) => Promise.resolve(_.merge({ id: faker.random.number(10) }, createObj)),
+        findAll: jest.fn(),
+        update: jest.fn(),
+    },
 };
