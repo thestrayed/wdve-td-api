f27bfa22aed1147806516c76e60b0f50a2ec990e
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-26 19:33:13 +0700

    add routes to app

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,13 @@ import config from 'config';
 import express from 'express';
 
 import { errorHandler } from '@middlewares';
+import router from '@routes';
 
 const app = express();
 
 app.set('port', config.get('wdve-td.port'));
+
+app.use(router);
 
 app.use(errorHandler);
 
