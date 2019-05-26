4ed046763af9148774a68b376086f9177f0ec0a9
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-26 19:35:03 +0700

    remove unnecessary try catch

--- server/controllers/place/all.ts
+++ server/controllers/place/all.ts
@@ -6,10 +6,6 @@ import { PlaceService } from '@services';
 const placeService = new PlaceService();
 
 export async function all(_req: Request, res: Response) {
-    try {
-        const places = await placeService.get();
-        res.status(HttpStatus.OK).json(places);
-    } catch (err) {
-        throw err;
-    }
+    const places = await placeService.get();
+    res.status(HttpStatus.OK).json(places);
 }
