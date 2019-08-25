6f01e6dfaaddd8c923d6792d5243fa9a37cf900b
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 20:00:50 +0700

    remove jest config for each test

--- server/tests/integration/jest.config.integration.js
+++
@@ -1,7 +0,0 @@
-const jestConfig = require('../../jest.config');
-
-jestConfig.collectCoverageFrom = [
-    'controllers/**/*.{js,ts}',
-];
-
-module.exports = jestConfig;
--- server/tests/unit/jest.config.unit.js
+++
@@ -1,9 +0,0 @@
-const jestConfig = require('../../jest.config');
-
-jestConfig.collectCoverageFrom = [
-    'erros/*.{js,ts}',
-    'lib/*.{js,ts}',
-    'services/*.{js,ts}'
-];
-
-module.exports = jestConfig;
