806ada547da15b4f18ec124f385e717b86133447
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 20:30:28 +0700

    add stub for jwt validation

--- server/tests/integration/controllers/place/index.test.ts
+++ server/tests/integration/controllers/place/index.test.ts
@@ -1,8 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+
 import { up, down } from '@tests/utilities/db';
 
 import { createPlaceIntegrationTest } from './create';
 import { getAllPlacesIntegrationTest } from './get-all';
 import { getByIdPlaceIntegrationTest } from './get-by-id';
+
+jest.mock('../../../../middlewares/jwt-validation', () => ({
+    jwtValidation: jest.fn().mockReturnValue((_req: Request, _res: Response, next: NextFunction) => {
+        next();
+    }),
+}));
 
 beforeAll(async () => {
     await up();
