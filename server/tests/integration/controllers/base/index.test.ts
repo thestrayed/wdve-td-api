6e7c1b3e4e7b87667098af037c6ec5a648c1b770
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 22:24:08 +0700

    add integration test for health check

---
+++ server/tests/integration/controllers/base/index.test.ts
@@ -0,0 +1,16 @@
+import HttpStatus from 'http-status';
+import request from 'supertest';
+
+import app from '../../../../app';
+
+const URL = '/api/health';
+
+describe('GET /api/health', () => {
+    it('should return OK status', async () => {
+        const result = await request(app)
+            .get(URL);
+
+        expect(result.body.data).toEqual({});
+        expect(result.status).toEqual(HttpStatus.OK);
+    });
+});
