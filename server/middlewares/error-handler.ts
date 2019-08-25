89caeef397f0f8cf10661685d3f99e419eb1c1c3
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 13:03:26 +0700

    explicitly add response type to function

--- server/middlewares/error-handler.ts
+++ server/middlewares/error-handler.ts
@@ -1,21 +1,30 @@
 import { NextFunction, Response, Request } from 'express';
 import HttpStatus from 'http-status';
-import _ from 'lodash';
 
-export function errorHandler(err: any, _req: Request, res: Response, next: NextFunction) {
+import { ASSERTION_ERROR, UNAUTHORIZED_ERROR } from '@constants/error';
+
+export function errorHandler(err: any, _req: Request, res: Response, next: NextFunction): Response {
     if (!err) {
         next();
         return;
     }
 
-    const errObj = _.pick(err, 'message', 'meta');
+    const { message, meta } = err;
     let { statusCode } = err;
 
     if (!statusCode) {
-        statusCode = err.constructor.name === 'AssertionError'
-            ? HttpStatus.BAD_REQUEST
-            : HttpStatus.BAD_GATEWAY;
+        switch (err.constructor.name) {
+            case ASSERTION_ERROR:
+                statusCode = HttpStatus.BAD_REQUEST;
+                break;
+            case UNAUTHORIZED_ERROR:
+                statusCode = HttpStatus.UNAUTHORIZED;
+                break;
+            default:
+                statusCode = HttpStatus.BAD_GATEWAY;
+                break;
+        }
     }
 
-    res.status(statusCode).json(errObj);
+    return res.status(statusCode).json({ message, meta });
 }
