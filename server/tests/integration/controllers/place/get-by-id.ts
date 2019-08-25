904ebe8884c2a348288b9614c1a2158b58a49ed7
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 22:17:29 +0700

    update get place by id integration with correct return model

--- server/tests/integration/controllers/place/get-by-id.ts
+++ server/tests/integration/controllers/place/get-by-id.ts
@@ -1,3 +1,4 @@
+import faker from 'faker';
 import HttpStatus from 'http-status';
 import request from 'supertest';
 
@@ -5,16 +6,27 @@ import app from '../../../../app';
 
 import ModelFactory from '@factory';
 import db from '@models';
-import { Place, PlaceModel, PlaceModelStatic } from '@typings/models/place';
+import { omitDateTimeFromModel } from '@tests/utilities/db';
+import {
+    PartialPlace,
+    Place,
+    PlaceModel,
+    PlaceModelStatic,
+} from '@typings/models/place';
 
 const LENGTH = 1;
-const URL = '/api/places/1';
+const URL = '/api/places';
 
 export const getByIdPlaceIntegrationTest = () => {
     let modelFactory: ModelFactory<PlaceModel, Place>;
+    const getByIdObj: PartialPlace = {
+        name: faker.name.title(),
+        latitude: parseFloat(faker.address.latitude()),
+        longitude: parseFloat(faker.address.longitude()),
+    };
 
     beforeEach(async () => {
-        modelFactory = new ModelFactory(db.Place as PlaceModelStatic);
+        modelFactory = new ModelFactory(db.Place as PlaceModelStatic, getByIdObj);
         await modelFactory.destroy();
     });
 
@@ -24,19 +36,21 @@ export const getByIdPlaceIntegrationTest = () => {
 
     it('should return error place not found', async () => {
         const result = await request(app)
-            .get(URL);
+            .get(`${URL}/${faker.random.number({ max: 1000 })}`);
 
         expect(result.body.message).toEqual('Place not found');
         expect(result.status).toEqual(HttpStatus.NOT_FOUND);
     });
 
-    it(`should return list of places with length of ${LENGTH}`, async () => {
+    it(`should return place`, async () => {
+        const [model] = await modelFactory.no(LENGTH).create();
+        const place = model.toJSON() as Place;
         await modelFactory.no(LENGTH).create();
         const result = await request(app)
-            .get(URL);
+            .get(`${URL}/${place.id}`);
 
         expect(
-            result.body.data
-        ).toBeTruthy;
+            omitDateTimeFromModel(result.body.data, ['id'])
+        ).toEqual(getByIdObj);
     });
 };
