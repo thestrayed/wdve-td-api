269bf661dfecbdbd8b42867457e242fafbec4828
Srettha Terananont <strayedpeople@gmail.com>
2019-08-25 13:09:43 +0700

    add new type for MiddlewareFuction

--- server/typings/utilities.ts
+++ server/typings/utilities.ts
@@ -1,7 +1,9 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 export type Method = 'GET' | 'DELETE' | 'HEAD' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH';
 export type MethodLowercase = 'get' | 'delete' | 'head' | 'options' | 'post' | 'put' | 'patch';
+
+export type MiddlewareFunction = (req: Request, res: Response, next: NextFunction) => void;
 
 export interface Route {
     path: string;
